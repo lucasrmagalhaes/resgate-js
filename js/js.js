571fde19b7773a037eff354cb417b97589b4c26d
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2020-10-22 23:27:12 -0300

    iniciando o js

---
+++ js/js.js
@@ -0,0 +1,10 @@
+function start() { // Inicio da função start()
+
+	$("#inicio").hide();
+	
+	$("#fundoGame").append("<div id='jogador'></div>");
+	$("#fundoGame").append("<div id='inimigo1'></div>");
+	$("#fundoGame").append("<div id='inimigo2'></div>");
+	$("#fundoGame").append("<div id='amigo'></div>");
+
+} // Fim da função start
