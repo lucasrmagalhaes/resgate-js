f55d86a40e550cba6d07e7d42dbac566077bfcba
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2020-10-23 01:26:05 -0300

    colisao

--- js/js.js
+++ js/js.js
@@ -42,6 +42,7 @@ function start()
         moveInimigo1();
         moveInimigo2();
         moveAmigo();
+        colisao();
 	}
 
     // Função que movimenta o fundo do jogo
@@ -148,5 +149,45 @@ function start()
                         
             }
         }
-    } 
+    }
+    
+    function colisao() 
+    {
+        var colisao1 = ($("#jogador").collision($("#inimigo1")));
+        
+        if (colisao1.length>0) {
+            
+            let inimigo1X = parseInt($("#inimigo1").css("left"));
+            let inimigo1Y = parseInt($("#inimigo1").css("top"));
+            
+            explosao1(inimigo1X,inimigo1Y);
+        
+            posicaoY = parseInt(Math.random() * 334);
+            
+            $("#inimigo1").css("left", 694);
+            $("#inimigo1").css("top", posicaoY);
+        }
+    }
+
+    function explosao1(inimigo1X,inimigo1Y) 
+    {
+        $("#fundoGame").append("<div id='explosao1'></div");
+        $("#explosao1").css("background-image", "url(imgs/explosao.png)");
+        
+        var div=$("#explosao1");
+        
+        div.css("top", inimigo1Y);
+        div.css("left", inimigo1X);
+        div.animate({width:200, opacity:0}, "slow");
+        
+        // Removendo a explosão.
+        var tempoExplosao = window.setInterval(removeExplosao, 1000);
+        
+        function removeExplosao() 
+        {
+            div.remove();
+            window.clearInterval(tempoExplosao);
+            tempoExplosao = null;
+        }        
+    }
 }
