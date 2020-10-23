94bce47a568e73bd9798eddbf756f68975130500
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2020-10-22 23:41:37 -0300

    anima 1, 2 e 3

--- js/js.js
+++ js/js.js
@@ -1,10 +1,9 @@
-function start() { // Inicio da função start()
-
-	$("#inicio").hide();
-	
-	$("#fundoGame").append("<div id='jogador'></div>");
-	$("#fundoGame").append("<div id='inimigo1'></div>");
+function start() 
+{
+    $("#inicio").hide();
+    
+	$("#fundoGame").append("<div id='jogador' class='anima1'></div>");
+	$("#fundoGame").append("<div id='inimigo1' class='anima2'></div>");
 	$("#fundoGame").append("<div id='inimigo2'></div>");
-	$("#fundoGame").append("<div id='amigo'></div>");
-
-} // Fim da função start
+	$("#fundoGame").append("<div id='amigo' class='anima3'></div>");
+}
