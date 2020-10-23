15d6d71989bca9d29335cd16e39344efbd55f245
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2020-10-23 01:13:41 -0300

    disparo definido

--- js/js.js
+++ js/js.js
@@ -12,7 +12,7 @@ function start()
     var posicaoY = parseInt(Math.random() * 334);
 
     var jogo = {} 
-
+    var podeAtirar = true
     var TECLA = 
     {
         W: 87,
@@ -74,7 +74,7 @@ function start()
         
         if (jogo.pressionou[TECLA.P]) 
         {    
-            //Chama função Disparo	
+            disparo()	
         }
     }
 
@@ -116,4 +116,37 @@ function start()
             $("#amigo").css("left",0)
         }   
     }
+
+    function disparo() {
+	
+        if (podeAtirar==true) 
+        {
+            podeAtirar=false;
+        
+            let topo = parseInt($("#jogador").css("top"))
+            let posicaoX= parseInt($("#jogador").css("left"))
+            let tiroX = posicaoX + 190;
+            let topoTiro= topo + 37;
+        
+            $("#fundoGame").append("<div id='disparo'></div");
+            $("#disparo").css("top", topoTiro);
+            $("#disparo").css("left", tiroX);
+        
+            var tempoDisparo = window.setInterval(executaDisparo, 30);
+        } 
+     
+        function executaDisparo() {
+            let posicaoX = parseInt($("#disparo").css("left"));
+            
+            $("#disparo").css("left", posicaoX + 15); 
+            
+            if (posicaoX > 900) { 
+                window.clearInterval(tempoDisparo);
+                tempoDisparo = null;
+                $("#disparo").remove();
+                podeAtirar=true;
+                        
+            }
+        }
+    } 
 }
